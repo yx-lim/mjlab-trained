bb27b45943650600a2b7bca6440638a1a27ccb00
yx-lim <yitxuenglim@gmail.com>
2026-08-18 15:42:41 -0700

    action rate weight 0.1->0.15

--- src/mjlab/tasks/tracking/config/g1_custom_contactrich/env_cfgs.py
+++ src/mjlab/tasks/tracking/config/g1_custom_contactrich/env_cfgs.py
@@ -9,7 +9,7 @@ other body geom is condim=1, i.e. frictionless. This env enables tangential
 friction (condim=3) and priority=1 on *all* collision geoms so the whole body
 has meaningful, robot-governed ground friction, then randomizes that friction.
 """
-
+from dataclasses import replace
 from mjlab.envs import ManagerBasedRlEnvCfg
 from mjlab.envs.mdp import dr
 from mjlab.managers.event_manager import EventTermCfg
@@ -63,5 +63,7 @@ def unitree_g1_custom_contactrich_flat_tracking_env_cfg(
       "shared_random": False,
     },
   )
+
+  cfg.rewards["action_rate_l2"] = replace(cfg.rewards["action_rate_l2"], weight=-0.15)
 
   return cfg
